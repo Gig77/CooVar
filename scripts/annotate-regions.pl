fe45715643939363f65441e49a8ba9ac4939bd13
Christian Frech <cfa24@sfu.ca>
2012-07-18 09:59:27 -0700

    script description

--- scripts/annotate-regions.pl
+++ scripts/annotate-regions.pl
@@ -1,5 +1,39 @@
 #!/usr/bin/perl
 
+#------------------------------------------------------------------------------------------------
+# DESCR: annotate GVs with genomic regions they overlap with
+# INPUT: 1) list of GVs in GVF format (VA_categorized_GVs.gvf output file) 
+#        2) genomic regions in GFF3 format (protein2genome.pl output)  
+# OUTPUT: GVs with annotated regions in GVF format
+#
+# SYNOPSIS:
+#
+#   ./annotate-regions.pl VA_categorized_GVs.gvf mapped_protein_domains.gff3 VA_categorized_GVs.w_regions.gvf
+#
+# Example region input lines (produced by protein2genome.pl):
+#
+#   V	Motif_homol	interpro	7642716	7642826	.	-	.	ID=INTERPRO:IPR003582(CDS:F07C4.11);p_start=3;p_end=39
+#   V	Motif_homol	interpro	7642542	7642670	.	-	.	ID=INTERPRO:IPR003582(CDS:F07C4.11);p_start=55;p_end=97
+#
+# These two entries specify that INTERPRO domain IPR003582 of protein-coding transcript CDS:4R79.1a maps
+# to two genomic regions, one at chromosome V from 7642716 to 7642826, and one at a neighboring location on
+# chromosome V from 7642542 to 7642670. The mapping of a protein domain to more than one genomic location
+# could be because a protein contains more than one copies of the domain (as shown in this case) or because
+# a single domain spans a splice junction and is therefore split-mapped to multiple genomic locations. 
+# Note that the format of the ID entry does not follow any special syntax and is just interpreted as 
+# the name of the genomic region that is copied over into the annotated GVF output file. Also 'p_start' 
+# and 'p_end' specifing start and end of this region in protein coordinates are optional.
+#
+# Example output line:
+#
+#   V	variant_analyzer	SNV	7642800	7642800	.	+	.	ID=snp_245887;Variant_seq=G;Reference_seq=A;Variant_type=synonymous_codon;Variant_effect=synonymous_codon 0 mRNA CDS:F07C4.11;Note=tgt>tgC_C>C_aa11_codon_loc3;region=INTERPRO:IPR003582(CDS:F07C4.11)
+#
+# Note the "region=INTERPRO:IPR003582(CDS:F07C4.11)" annotation at the end of the line,
+# which was not present before in the GVF input and specifies with which genomic regions this paritcular GV 
+# (a SNP in this case) overlaps. If multiple regions are found to overlap with a GV
+# they will be separated by a comma. 
+#------------------------------------------------------------------------------------------------
+
 use strict;
 use Set::IntervalTree;
 
